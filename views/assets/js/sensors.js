41f60aff480f634924b288d856470c79d9953bb7
Andrzej Król <andrzej.krol@makingwaves.no>
2017-04-29 14:58:17 +0200

    unecesery files removed

--- views/assets/js/sensors.js
+++ views/assets/js/sensors.js
@@ -1,20 +1,45 @@
 function getSensors() {
-      $.get("/sensor", function(data) {
-          var resultHtml = "<table class='highlight' id='resultTable'><thead><tr><th>Nazwa</th><th>Numer</th><th>Numer stanowiska</th><th>Opis</th></tr></thead><tbody></tbody></table>"
-          $(".result").empty();
-          $(".result").append(resultHtml);
-          $.each(data.sensors, function(index, value) {
-              $('#resultTable').append('<tr><td>' + value.sensorsName + '</td><td>' + value.stationsNumber + '</td><td>' + value.sensorsNumber + '</td><td>' + value.sensorsDesc + '</td></tr>');
-          });
-      }).done(function() {
-          $("#resultTable").tablesorter();
-      })
-  }
+    $.get("/sensor", function(data) {
+        var resultHtml = "<table class='highlight' id='resultTable'><thead><tr><th>Nazwa</th><th>Numer stanowiska</th><th>Numer czujnika</th><th>Opis</th></tr></thead><tbody></tbody></table>"
+        $(".result").empty();
+        $(".result").append(resultHtml);
+        $.each(data.sensors, function(index, value) {
+            $('#resultTable').append('<tr><td>' + value.sensorsName + '</td><td>' + value.stationsNumber + '</td><td>' + value.sensorsNumber + '</td><td>' + value.sensorsDesc + '</td></tr>');
+        });
+    }).done(function() {
+        $("#resultTable").tablesorter();
+    })
+}
+
+function getSensors(stationId) {
+    query = "/sensor";
+    if (stationId) {
+        query += "?station="
+        query += stationId;
+    }
+    $.get(query, function(data) {
+        var resultHtml = "<table class='highlight' id='resultTable'><thead><tr><th>Nazwa</th><th>Numer stanowiska</th><th>Numer czujnika</th><th>Opis</th></tr></thead><tbody></tbody></table>"
+        $(".result").empty();
+        $(".result").append(resultHtml);
+        $.each(data.sensors, function(index, value) {
+            $('#resultTable').append('<tr><td>' + value.sensorsName + '</td><td>' + value.stationsNumber + '</td><td>' + value.sensorsNumber + '</td><td>' + value.sensorsDesc + '</td></tr>');
+        });
+    }).done(function() {
+        $("#resultTable").tablesorter();
+    })
+}
 
 
-function validateSensorNumber(stationNumber,number) {
+
+
+function validateSensorNumber(stationNumber, number) {
     var result = [];
-    $.get("/sensor?station="+stationNumber, function(data) {
+    query = "/sensor";
+    if (stationNumber) {
+        query += "?station="
+        query += stationNumber;
+    }
+    $.get(query, function(data) {
         $.each(data.sensors, function(index, value) {
             var y = value.sensorsNumber;
             result[index] = y;
